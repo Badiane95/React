cf19ce00c9650eb0bc2de132e5101bb2108bd49b
Badiane95 <badiane.falou95@gmail.com>
2025-02-10 17:20:07 +0100

    hh

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./Home";
+import Blogs from "./Blogs";
+import Contact from "./Conctact";
+import NoPage from "./NoPage";
+
+
+function App() {
+  return (
+    <header>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  ;
+
+
+      </header>
+    
+  );
+}
+
+export default App;
